904fe3d14030d2accf44706f027d38057683792d
Anna <ann.kanska@gmail.com>
2020-01-13 15:37:12 +0100

    created server

---
+++ server/server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const socketio = require("socket.io");
+const http = require("http");
+
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+server.listen(PORT, () => {
+  console.log(`Server has started on port ${PORT}`);
+});
+
+io.on("connection", function(socket) {
+  socket.emit("news", { hello: "world" });
+  socket.on("my other event", function(data) {
+    console.log(data);
+  });
+});
