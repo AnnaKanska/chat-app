a7bcf5299205188e4dfab0561c576dc860c8ba77
Anna <ann.kanska@gmail.com>
2020-01-13 12:08:56 +0100

    created chatView component

--- src/components/chatView.jsx
+++ src/components/chatView.jsx
@@ -1,5 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function ChatView({ name }) {
-  return <div>chat view {name}</div>;
+  const [textValue, setTextValue] = useState("");
+  console.log(textValue);
+  return (
+    <div className="chatView">
+      <div className="messageDisplay"></div>
+      <form onSubmit={() => {}} className="textEntry">
+        <p className="userName">{name}</p>
+        <input
+          className="messageInput"
+          type="text"
+          onChange={e => setTextValue(e.target.value)}
+          value={textValue}
+        />
+        <button className="sendBtn" type="submit">
+          send
+        </button>
+      </form>
+    </div>
+  );
 }
